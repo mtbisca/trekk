ff7bdcfa12d959880dcf090211478cfcc07225a6
vitor-alves <vitoralves842@gmail.com>
2018-06-26 01:26:56 -0300

    MAJOR changes in overall structure. Added vue-router. Added login page.

---
+++ front-end/DashboardWindow.js
@@ -0,0 +1,288 @@
+const DashboardWindow = { 
+	data: function() {
+		return {
+          tasks: [],
+
+          messages: [
+            {
+              text: "Ow vc sabe do joaozinho?",
+              owner: ["chat", "self"]
+            },
+            {
+              text: "Sei nao",
+              owner: ["chat", "friend"]
+            },
+            {
+              text: "Pq?",
+              owner: ["chat", "friend"]
+            },
+            {
+              text: "To travado em uma dependencia dele",
+              owner: ["chat", "self"]
+            },
+          ],
+          newMessageText: "",
+          teamMembers: ["Pedro", "João", "Maria"],
+          createTaskData: {
+            taskId: 3,
+            taskTitle: "",
+            taskDescription: "",
+            taskDeadline: "",
+            checkedTeamMembers: [],
+            checkedDependencies:[],
+            subItemsList: [],
+            currentSubItem: ""
+          },
+          inCreateMode: false
+		}	
+	},
+	created: function () { 
+ axios.get('http://localhost:8000/taskcontroller/getAll')
+    .then(response => {
+      // JSON responses are automatically parsed.
+      console.log(response.data)
+      this.tasks = response.data
+    })
+    .catch(e => {
+      console.log(e)
+    })
+ },
+      
+	methods: {
+        "toggleExpandClass": function (task) {
+          task.isExpanded = !task.isExpanded;
+        },
+        "calculateProgress": function (task) {
+          task.progress = task.checkedItems.length * 100 / task.checklistItems.length ;
+          this.setProgressClass(task);
+        },
+        "setProgressClass": function (task) {
+          task.progressClasses.pop();
+          if(task.progress < 34) {
+            task.progressClasses.push("is-danger");
+          } else if (task.progress < 67) {
+            task.progressClasses.push("is-warning");
+          } else {
+            task.progressClasses.push("is-success");
+          }
+        },
+        "getDependencieTitle": function (id) {
+          for (let i = 0; i < this.tasks.length; i++) {
+            if (this.tasks[i].id == id) {
+              return this.tasks[i].title;
+            }
+          }
+        },
+        "getDependencieProgress": function (id) {
+          for (let i = 0; i < this.tasks.length; i++) {
+            if (this.tasks[i].id == id) {
+              return this.tasks[i].progress;
+            }
+          }
+        },
+        "getDependencieProgressClasses": function (id) {
+          for (let i = 0; i < this.tasks.length; i++) {
+            if (this.tasks[i].id == id) {
+              return this.tasks[i].progressClasses;
+            }
+          }
+        },
+
+        "toggleCreateMode": function () {
+          this.inCreateMode = !this.inCreateMode;
+        },
+
+        "sendMessage": function () {
+          this.messages.push({
+            text: this.newMessageText,
+            owner: ["chat", "self"]
+          });
+          this.newMessageText = "";
+        },
+
+        "addNewSubItem": function () {
+          this.createTaskData.subItemsList.push(this.createTaskData.currentSubItem);
+          this.createTaskData.currentSubItem = "";
+        },
+
+       "createTask": function () {
+          axios.post('http://localhost:8000/taskcontroller/inserir', {
+              title: this.createTaskData.taskTitle,
+              description: this.createTaskData.taskDescription,
+              deadline: this.createTaskData.taskDeadline,
+              progress: 0,
+              progressClasses: ["progress", "is-danger"],
+              dependencies: this.createTaskData.checkedDependencies,
+              creator: 0,
+              members: this.createTaskData.checkedTeamMembers.length,
+              checklistItems: this.createTaskData.subItemsList,
+              checkedItems: [],
+              isExpanded: false
+            })
+            .then(function (response) {
+              console.log(response);
+              this.tasks.push(response.data)
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+
+            // clear previous create data
+            this.createTaskData.taskTitle = "";
+            this.createTaskData.taskDescription = "";
+            this.createTaskData.taskDeadline = "";
+            this.createTaskData.checkedTeamMembers = [];
+            this.createTaskData.checkedDependencies = [];
+            this.createTaskData.subItemsList = [];
+            this.createTaskData.currentSubItem = "";
+            this.toggleCreateMode();
+        }
+      },
+
+	template: `<div id = 'dashboard-base'>
+    <nav class="navbar is-fixed-top is-success" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" @click="() => this.inCreateMode = false">
+          <img src="./TrekkLogo.png" alt="Welcome to Trekk!">
+          <p class="trekk-title">Trekk</p>
+        </a>
+      </div>
+      <div class="navbar-end">
+        <a class="navbar-item is-pulled-right" v-show="!inCreateMode">
+          <span class="icon" @click="toggleCreateMode()">
+            <i class="fas fa-plus-square fa-2x" aria-hidden="true"></i>
+          </span>
+        </a>
+      </div>
+    </nav>
+    <div class="chat-container" v-show="!inCreateMode">
+      <article class="message is-medium is-success">
+        <div class="message-header">
+          <p>Chat</p>
+        </div>
+        <div class="chatbox">
+          <div class="chatlogs">
+            <div v-bind:class="message.owner" v-for="message in messages">
+              <div class="user-photo"></div>
+              <p class="chat-message">{{message.text}}</p>
+            </div>
+          </div>
+          <div class="chat-form">
+            <textarea v-model="newMessageText"></textarea>
+            <div class="control">
+              <button class="button is-primary is-fullwidth" @click="sendMessage()">Enviar</button>
+            </div>
+          </div>
+        </div>
+      </article>
+    </div>
+    <div class="cards-container">
+      <article class="message is-medium is-success" v-for="task in tasks">
+        <div class="message-header" v-on:click="toggleExpandClass(task)">
+          <p>{{task.title}}</p>
+          <p>Deadline: {{task.deadline}}</p>
+        </div>
+        <div class="message-body">
+          <div class="message-body-left">
+            <progress v-bind:class="task.progressClasses" v-bind:value="task.progress" max="100">90%</progress>
+            <div v-show="task.isExpanded">
+              <p>{{task.description}}</p>
+              <nav class="panel" v-show="task.checklistItems.length > 0">
+                <p class="panel-heading">
+                  Task Progress
+                </p>
+                <div class="panel-block" v-for="item in task.checklistItems">
+                  <label class="checkbox">
+                    <input type="checkbox" v-bind:value="item" v-model="task.checkedItems" @change="calculateProgress(task)">
+                    {{item}}
+                  </label>
+                </div>
+              </nav>
+            </div>
+          </div>
+          <div class="message-body-right">
+            <div class="team-member-container">
+              <img class="team-member-icon task-creator" src="bot.png"></img>
+              <img class="team-member-icon" src="bot.png" v-for="i in task.members - 1"></img>
+            </div>
+            <div class="task-dependencies-container" v-show="task.isExpanded">
+              <nav class="panel" v-show="task.dependencies.length > 0">
+                <p class="panel-heading">Task Dependencies: </p>
+                <div class="task-dependencie panel-block" v-for="id in task.dependencies">
+                   <p>{{getDependencieTitle(id)}}</p>
+                   <progress v-bind:class="getDependencieProgressClasses(id)" v-bind:value="getDependencieProgress(id)" max="100">90%</progress>
+                 </div>
+               </nav>
+             </div>
+          </div>
+        </div>
+      </article>
+    </div>
+    <div class="form-container" v-show="inCreateMode">
+      <article class="message is-medium is-success">
+        <div class="message-header">
+          <p>Nova Task</p>
+        </div>
+        <div class="message-body">
+          <div class="field">
+            <label class="label">Titulo</label>
+            <div class="control">
+              <input class="input" type="text" placeholder="digite o nome da tarefa" v-model="createTaskData.taskTitle">
+            </div>
+          </div>
+          <div class="field">
+            <label class="label">Descriçao</label>
+            <div class="field-body">
+              <div class="field">
+                <div class="control">
+                  <textarea class="textarea" placeholder="Descriçao da tarefa" v-model="createTaskData.taskDescription"></textarea>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <label class="label">Membros</label>
+          <nav class="panel">
+            <div class="panel-block" v-for="member in this.teamMembers">
+              <label class="checkbox">
+                <input type="checkbox" v-bind:value="member" v-model="createTaskData.checkedTeamMembers">
+                {{member}}
+              </label>
+            </div>
+          </nav>
+          <label class="label">Dependencias</label>
+          <nav class="panel">
+            <div class="panel-block" v-for="task in this.tasks">
+              <label class="checkbox">
+                <input type="checkbox" v-bind:value="task.id" v-model="createTaskData.checkedDependencies">
+                {{task.title}}
+              </label>
+            </div>
+          </nav>
+          <div class="subitems-list">
+            <li v-for="item in createTaskData.subItemsList">{{item}}</li>
+          </div>
+          <div class="field has-addons">
+            <div class="control">
+              <input class="input" type="text" placeholder="Nova subtarefa" v-model="createTaskData.currentSubItem">
+            </div>
+            <div class="control">
+              <a class="button is-primary" @click="addNewSubItem()">
+                +
+              </a>
+            </div>
+          </div>
+          <div class="field">
+            <label class="label">Deadline</label>
+            <div class="control">
+              <input class="input" type="text" placeholder="dd/mm/aaaa" v-model="createTaskData.taskDeadline">
+            </div>
+          </div>
+          <div class="control">
+            <button class="button is-primary" @click="createTask()">Criar</button>
+            <button class="button is-danger" @click="toggleCreateMode()">Cancel</button>
+          </div>
+        </div>
+      </article>
+    </div>
+  </div>` }
